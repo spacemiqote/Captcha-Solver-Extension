ed9d7e4a8da5f497d37073f3ef1bb8ac500aed04
dumbie <s1311132009@nutc.edu.tw>
2023-06-24 08:10:39 +0800

    [A] 001
    Upload v1.5 extension full source code.

---
+++ background.js
@@ -0,0 +1,71 @@
+importScripts(chrome.runtime.getURL('scripts/tf.es2017.js'));
+
+const CHARACTERS = '0123456789abcdefghijklmnopqrstuvwxyz';
+
+let tfmodel;
+
+async function loadModel() {
+  try {
+    tfmodel = await tf.loadLayersModel(chrome.runtime.getURL('model/model.json'));
+    console.log('模型載入完成!');
+  } catch (error) {
+    console.error('模型載入失敗! 錯誤訊息:', error);
+  }
+}
+
+async function solveCaptcha(captchaImage) {
+  try {
+    const response = await fetch(captchaImage);
+    const blob = await response.blob();
+    const imgBitmap = await createImageBitmap(blob);
+
+    let tensor = tf.browser.fromPixels(imgBitmap)
+      .resizeNearestNeighbor([30, 90])
+      .toFloat()
+      .div(255.0);
+    tensor = tensor.mul([0.2989, 0.5870, 0.1140]).sum(-1).expandDims(-1);
+
+    const prediction = tfmodel.predict(tensor.expandDims());
+    const output = await Promise.all(prediction.map(p => p.data()));
+
+    const result = output.map(array => CHARACTERS[array.indexOf(Math.max(...array))]).join('');
+
+    tensor.dispose(); 
+    return { status: 'solved', captcha: result };
+  } catch (error) {
+    console.error("錯誤訊息:", error);
+    return { status: 'error', message: '無法辨識驗證碼' };
+  }
+}
+
+chrome.webNavigation.onBeforeNavigate.addListener(() => {
+  console.debug('喚醒擴展!');
+});
+
+chrome.runtime.onStartup.addListener(() => {
+  console.log('初始化擴展!');
+});
+
+self.addEventListener('activate', () => {
+  console.log('啟用擴展!');
+});
+
+self.addEventListener('install', async () => {
+  await loadModel();
+  self.skipWaiting();
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'solve_captcha') {
+    if (tfmodel) {
+      solveCaptcha(request.image).then(sendResponse);
+    } else {
+      loadModel().then(() => {
+        solveCaptcha(request.image).then(sendResponse);
+      });
+    }
+    return true;
+  }
+});
+
+loadModel();
---
+++ scripts/content.js
@@ -0,0 +1,36 @@
+function captureAndSendCaptcha() {
+    var xpath = "/html/body/div[1]/div[2]/form/div[3]/table/tbody/tr[1]/td/table/tbody/tr[3]/td[1]/img";
+    var element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+    if (element) {
+        if (element.complete) {
+            sendCaptcha(element);
+        } else {
+            element.addEventListener('load', () => {
+                sendCaptcha(element);
+            });
+        }
+    }
+}
+
+function sendCaptcha(element) {
+    var canvas = document.createElement("canvas");
+    canvas.width = element.width;
+    canvas.height = element.height;
+    var context = canvas.getContext("2d");
+    context.drawImage(element, 0, 0);
+    var captchaImage = canvas.toDataURL("image/jpeg", 1);
+
+    chrome.runtime.sendMessage({ action: "solve_captcha", image: captchaImage }, (response) => {
+        if (response.status === "solved") {
+            var inputElement = document.getElementById("ContentPlaceHolder1_ValidationCode");
+            if (inputElement) {
+                inputElement.value = response.captcha;
+            }
+        } else {
+            console.log('驗證碼辨識失敗');
+        }
+    });
+}
+
+window.addEventListener('load', captureAndSendCaptcha);

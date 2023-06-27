5e209ada9e2f68186fff526e0d828429353ba12c
dumbie <s1311132009@nutc.edu.tw>
2023-06-27 10:32:24 +0800

    [U] 021
    Improve model accuracy by increasing captcha count to 5443 samples. Resolves #1

--- background.js
+++ background.js
@@ -18,25 +18,59 @@ async function solveCaptcha(captchaImage) {
     const response = await fetch(captchaImage);
     const blob = await response.blob();
     const imgBitmap = await createImageBitmap(blob);
-
     let tensor = tf.browser.fromPixels(imgBitmap)
       .resizeNearestNeighbor([30, 90])
       .toFloat()
       .div(255.0);
     tensor = tensor.mul([0.2989, 0.5870, 0.1140]).sum(-1).expandDims(-1);
-
     const prediction = tfmodel.predict(tensor.expandDims());
     const output = await Promise.all(prediction.map(p => p.data()));
+    let sumConfidence = 0;
+    let characters = output.map((array, index) => {
+      const max = Math.max(...array);
+      sumConfidence += max;
+      return {char: CHARACTERS[array.indexOf(max)], confidences: array};
+    });
+    const avgConfidence = (sumConfidence / output.length) * 100;
+    for (let i = 0; i < characters.length - 1; i++) {
+      if (characters[i].char === characters[i + 1].char) {
+        const confidences1 = characters[i].confidences.slice().sort((a, b) => b - a);
+        const confidences2 = characters[i + 1].confidences.slice().sort((a, b) => b - a);
 
-    const result = output.map(array => CHARACTERS[array.indexOf(Math.max(...array))]).join('');
-
-    tensor.dispose(); 
+        if (confidences1[0] > confidences2[0]) {
+          characters[i + 1].char = CHARACTERS[characters[i + 1].confidences.indexOf(confidences2[1])];
+        } else {
+          characters[i].char = CHARACTERS[characters[i].confidences.indexOf(confidences1[1])];
+        }
+      }
+    }
+    for (let i = 0; i < characters.length - 1; i++) {
+      if (characters[i].char === characters[i + 1].char) {
+        const confidences1 = characters[i].confidences.slice().sort((a, b) => b - a);
+        const confidences2 = characters[i + 1].confidences.slice().sort((a, b) => b - a);
+        let alternativeIndex = 2;
+        let newChar;
+        do {
+          newChar = CHARACTERS[characters[i + 1].confidences.indexOf(confidences2[alternativeIndex])];
+          alternativeIndex++;
+        } while (newChar === characters[i + 1].char && alternativeIndex < confidences2.length);
+        if (confidences1[0] > confidences2[0]) {
+          characters[i + 1].char = newChar;
+        } else {
+          characters[i].char = newChar;
+        }
+      }
+    }
+    const result = characters.map(c => c.char).join('');
+    tensor.dispose();
     return { status: 'solved', captcha: result };
   } catch (error) {
     console.error("錯誤訊息:", error);
     return { status: 'error', message: '無法辨識驗證碼' };
   }
 }
+
+
 
 chrome.webNavigation.onBeforeNavigate.addListener(() => {
   console.debug('喚醒擴展!');
